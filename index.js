92aad5bf6c09d3790f1d6a375b2c652a0edf70a8
Walmyr Filho <limaesilva@qelp.com>
2021-10-11 15:38:54 +0200

    Better name function and variable

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 function isAnagram(arg1, arg2) {
   if (Array.isArray(arg1)) {
-    cleanArray = arg1.map(arg => cleanString(arg))
-    return cleanArray.every(item => item === cleanArray[0])
+    formalizedArray = arg1.map(arg => formalizeString(arg))
+    return formalizedArray.every(item => item === formalizedArray[0])
   } else if (typeof arg1 === 'string' && typeof arg2 === 'string') {
-    return cleanString(arg1) === cleanString(arg2)
+    return formalizeString(arg1) === formalizeString(arg2)
   } else {
     throw new Error(`Arg1 should be or an array of strings, or a string. If the later, arg2 should also be provided, and it should also be a string. Arg1: ${arg1}, Arg2: ${arg2}`)
   }
 }
 
-function cleanString(str) {
+function formalizeString(str) {
   return str.replace(/[^\w]/g).toLowerCase().split('').sort().join()
 }
 
